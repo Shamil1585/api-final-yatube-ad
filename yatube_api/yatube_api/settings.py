dc90528504c79fa0264c2938496004006d881d49
abdulkhalikov05 <abdulkhalikov_05@bk.ru>
2026-03-22 23:26:30 +0400

    Все исправления

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -96,8 +96,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': None,  # Отключаем глобальную пагинацию
 }
 
 SIMPLE_JWT = {
